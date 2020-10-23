45d33ed1af4f2826c551b915e1b4c71b022d0b8d
vaibhavpallod <vaibhavpallod@gmail.com>
2020-10-23 20:57:59 +0530

    Grid added to event page

--- lib/commons/collap_nav_dr.dart
+++ lib/commons/collap_nav_dr.dart
@@ -1,4 +1,4 @@
-import 'package:credenz20/nav_pages/Contact%20Us.dart';
+import 'package:credenz20/BottomNav/Contact.dart';
 import 'package:credenz20/nav_pages/about_us.dart';
 import 'package:credenz20/nav_pages/editprofile.dart';
 import 'package:credenz20/nav_pages/myevents.dart';
@@ -116,7 +116,7 @@ class MenuDrawer extends StatelessWidget {
                     onTap: () {
                       //Navigator.of(context).pop();
                       Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) => ContactUs()));
+                          builder: (BuildContext context) => Contact()));
                     },
                   ),
                   ListTile(
