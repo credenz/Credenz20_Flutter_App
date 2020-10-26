5940482817d50cd53802d7433bfd999b134044c5
vaibhavpallod <vaibhavpallod@gmail.com>
2020-10-26 20:45:36 +0530

    Grid added to event page

--- lib/BottomNav/Events.dart
+++ lib/BottomNav/Events.dart
@@ -1,6 +1,9 @@
+import 'package:credenz20/models/Event_Des_Model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import '../theme.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+import 'package:toast/toast.dart';
 
 class Events extends StatefulWidget {
   @override
@@ -32,8 +35,29 @@ class _EventsState extends State<Events> {
           crossAxisCount: 4,
           itemCount: 12,
           itemBuilder: (BuildContext context, int index) => new Container(
+
             color: drawerBackgroundColor,
             child: Container(
+              child: InkWell(
+                onTap: (){
+
+                  Toast.show('$index', context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => EventDes()),
+                  );
+                  // Fluttertoast.showToast(
+                  //
+                  //     msg: '$index',
+                  //     toastLength: Toast.LENGTH_SHORT,
+                  //     gravity: ToastGravity.CENTER,
+                  //     timeInSecForIosWeb: 1,
+                  //     backgroundColor: Colors.red,
+                  //     textColor: Colors.white,
+                  //     fontSize: 16.0
+                  // );
+                },
+              ),
               width: 10,
               height: 10,
               decoration: BoxDecoration(
