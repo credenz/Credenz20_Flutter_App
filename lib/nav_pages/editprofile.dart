b20a56ec2831047e57ed91659d367999c732197b
Durvesh Malpure <62510468+durvesh8@users.noreply.github.com>
2020-10-29 10:09:21 +0530

    refractoring imports

--- lib/nav_pages/editprofile.dart
+++ lib/nav_pages/editprofile.dart
@@ -1,4 +1,4 @@
-import 'file:///C:/Users/asus/Documents/GitHub/Credenz20_Flutter_App/lib/constants/theme.dart';
+import 'package:credenz20/constants/theme.dart';
 import 'package:flutter/material.dart';
 class EditProfile extends StatelessWidget {
   @override
