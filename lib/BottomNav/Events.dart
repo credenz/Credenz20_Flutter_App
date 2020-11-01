9e314c7c8323a5bad4383c57abd778350f7383ec
Durvesh Malpure <62510468+durvesh8@users.noreply.github.com>
2020-11-01 13:54:17 +0530

    updated pubspec.lock

--- lib/BottomNav/Events.dart
+++ lib/BottomNav/Events.dart
@@ -1,84 +1,109 @@
-import 'package:credenz20/models/Event_Des_Model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:credenz20/constants/theme.dart';
-
-// import 'package:fluttertoast/fluttertoast.dart';
-import 'package:toast/toast.dart';
+import 'package:circle_wheel_scroll/circle_wheel_scroll_view.dart';
 import '../constants/theme.dart';
 
 class Events extends StatefulWidget {
   @override
   _EventsState createState() => _EventsState();
 }
-
+List<AssetImage> eventimages = [
+  AssetImage('images/clash1.png'),
+  AssetImage('images/contraption.png'),
+  AssetImage('images/cretronix.png'),
+  AssetImage('images/enigma.png'),
+  AssetImage('images/datawizwhite.png'),
+  AssetImage('images/nth.png'),
+  AssetImage('images/paper.png'),
+  AssetImage('images/pixelate.png'),
+  AssetImage('images/quiz.png'),
+  AssetImage('images/revb.png'),
+  AssetImage('images/roboliga.png'),
+  AssetImage('images/xodia.png'),
+];
 class _EventsState extends State<Events> {
-  List<AssetImage> eventimages = [
-    AssetImage('images/clash1.png'),
-    AssetImage('images/contraption.png'),
-    AssetImage('images/cretronix.png'),
-    AssetImage('images/enigma.png'),
-    AssetImage('images/datawizwhite.png'),
-    AssetImage('images/nth.png'),
-    AssetImage('images/paper.png'),
-    AssetImage('images/pixelate.png'),
-    AssetImage('images/quiz.png'),
-    AssetImage('images/revb.png'),
-    AssetImage('images/roboliga.png'),
-    AssetImage('images/xodia.png'),
-  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: new Padding(
         padding: const EdgeInsets.all(4),
-        child: new StaggeredGridView.countBuilder(
-          crossAxisCount: 4,
-          itemCount: 12,
-          itemBuilder: (BuildContext context, int index) => new Container(
+        child: WheelExample(),
+        // new StaggeredGridView.countBuilder(
+        //   crossAxisCount: 4,
+        //   itemCount: 12,
+        //   itemBuilder: (BuildContext context, int index) => new Container(
+        //     color: drawerBackgroundColor,
+        //     child: Container(
+        //       width: 10,
+        //       height: 10,
+        //       decoration: BoxDecoration(
+        //         shape: BoxShape.circle,
+        //
+        //         image: DecorationImage(
+        //           // fit: BoxFit.fill,
+        //         image: eventimages[index],
+        //         fit: BoxFit.fitHeight,
+        //         ),
+        //
+        //       ),
+        //     ),
+        //   ),
+        //   staggeredTileBuilder: (int index) =>
+        //       new StaggeredTile.count(2, index.isEven ? 2 : 1),
+        //   mainAxisSpacing: 4.0,
+        //   crossAxisSpacing: 4.0,
+        // ),
+      ),
+    );
+  }
+}
+
+class WheelExample extends StatelessWidget {
+  Widget _buildItem(int i) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            width: 325,
+            padding: EdgeInsets.all(20),
             color: drawerBackgroundColor,
             child: Container(
-              child: InkWell(
-                onTap: (){
-
-                  Toast.show('$index', context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => EventDes()),
-                  );
-                  // Fluttertoast.showToast(
-                  //
-                  //     msg: '$index',
-                  //     toastLength: Toast.LENGTH_SHORT,
-                  //     gravity: ToastGravity.CENTER,
-                  //     timeInSecForIosWeb: 1,
-                  //     backgroundColor: Colors.red,
-                  //     textColor: Colors.white,
-                  //     fontSize: 16.0
-                  // );
-                },
-              ),
-              width: 10,
-              height: 10,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-
                 image: DecorationImage(
-                  // fit: BoxFit.fill,
-                image: eventimages[index],
-                fit: BoxFit.fitHeight,
-                ),
-
+                        image: eventimages[i],
+                        fit: BoxFit.fitHeight,
               ),
             ),
           ),
-          staggeredTileBuilder: (int index) =>
-              new StaggeredTile.count(2, index.isEven ? 2 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
         ),
+      )
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Center(
+          child: Container(
+            // height: 260,
+            // width: 160,
+            child: CircleListScrollView(
+              physics: CircleFixedExtentScrollPhysics(),
+              axis: Axis.vertical,
+              itemExtent: 250,
+              children: List.generate(12, _buildItem),
+              radius: MediaQuery.of(context).size.width * 0.8,
+              // onSelectedItemChanged: (int index) => print('Current index: $index'),
+            ),
+          ),
+        ),
+      );
   }
 }
