b77604ba6458e6c8d50b3a0f159c5c381beee79d
atharvasaraf123 <sarafatharva123@gmail.com>
2020-12-20 23:48:12 +0530

    Notification UI changes

--- lib/BottomNav/Profile.dart
+++ lib/BottomNav/Profile.dart
@@ -1,4 +1,9 @@
+import 'package:credenz20/constants/API.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+import '../loginPage.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -8,11 +13,37 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
 
 
+  final storage=FlutterSecureStorage();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getUserInfo();
+  }
 
+  getUserInfo()async{
+    String url=userProfileUrl;
+    String accToken=await storage.read(key: "accToken");
+    String username=await storage.read(key: 'username');
+    if(username==null||accToken==null){
+      Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>Login()));
+    }else {
+      Map<String, String>headers = {
+        "Authorization": "Bearer $accToken"
+      };
+      http.Response response=await http.get(url,headers: headers);
+      if(response.statusCode==200){
+                print(url);
+                print(headers);
+                print(response.body);
+                print(response.statusCode);
+      }else{
+        print(url);
+        print(headers);
+        print(response.body);
+        print(response.statusCode);
+      }
+    }
   }
 
 
