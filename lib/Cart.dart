11bbbbec65681ae49cffdc73c1bfc645c0d9686a
tanuj223 <tanujagrawal223@gmail.com>
2021-01-13 15:46:23 +0530

    Cart changes design

--- lib/Cart.dart
+++ lib/Cart.dart
@@ -8,6 +8,7 @@ import 'package:credenz20/loginPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
 // import 'package:razorpay_flutter/razorpay_flutter.dart';
 import 'commons/slide_drawer.dart';
 import 'constants/theme.dart';
@@ -20,9 +21,10 @@ class Cart extends StatefulWidget {
 
 class _CartState extends State<Cart> {
   final storage = FlutterSecureStorage();
-  int sum=0;
+  int sum = 0;
   List list;
   bool load = true;
+
   // Razorpay _razorpay;
   final children1 = <Widget>[];
 
@@ -35,25 +37,24 @@ class _CartState extends State<Cart> {
     // _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
     // _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
     getEventPrices();
-    sum=0;
+    sum = 0;
   }
 
-
-  getEventPrices()async{
-    prices1=new Map();
-    String url=allEventUrl;
-    http.Response response=await http.get(url);
-    if(response.statusCode==200){
-      List list=jsonDecode(response.body) as List;
-      for(int i=0;i<list.length;i++){
-        prices1[list[i]['event_name']]=list[i]['event_price'];
+  getEventPrices() async {
+    prices1 = new Map();
+    String url = allEventUrl;
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      List list = jsonDecode(response.body) as List;
+      for (int i = 0; i < list.length; i++) {
+        prices1[list[i]['event_name']] = list[i]['event_price'];
       }
       await loadCart();
     }
   }
 
   loadCart() async {
-    sum=0;
+    sum = 0;
     children1.clear();
     list = List();
     for (int i = 0; i < 12; i++) {
@@ -61,7 +62,9 @@ class _CartState extends State<Cart> {
       if (pre) {
         String eventName = await storage.read(key: '$i');
         list.add(eventName);
-        prices1.containsKey(eventName)?sum+=prices1[eventName].toInt():sum+=120;
+        prices1.containsKey(eventName)
+            ? sum += prices1[eventName].toInt()
+            : sum += 120;
       }
     }
     for (var i = 0; i < list.length; i++) {
@@ -77,11 +80,14 @@ class _CartState extends State<Cart> {
               shape: BoxShape.circle,
               color: drawerBackgroundColor,
             ),
-            child: Image.asset(eventimages[eventName.indexOf(list[i],0)].assetName),
+            child: Image.asset(
+                eventimages[eventName.indexOf(list[i], 0)].assetName),
           ),
         ),
       ));
-      children1.add(new SizedBox(width: 10,));
+      children1.add(new SizedBox(
+        width: 10,
+      ));
     }
     setState(() {
       load = false;
@@ -99,7 +105,7 @@ class _CartState extends State<Cart> {
     // await storage.delete(key: null);
   }
 
- /* void _handlePaymentSuccess(PaymentSuccessResponse response) {
+  /* void _handlePaymentSuccess(PaymentSuccessResponse response) {
     Fluttertoast.showToast(
         msg: "SUCCESS: " + response.paymentId, timeInSecForIosWeb: 4);
   }
@@ -119,9 +125,12 @@ class _CartState extends State<Cart> {
     String username = await storage.read(key: 'username');
     String accToken = await storage.read(key: "accToken");
     if (username != null && accToken != null) {
-      int amt=0;
+      int amt = 0;
       for (int i = 0; i < list.length; i++) {
-        String url = baseUrl + username + '/${list[i].toString().toLowerCase()}';///rc
+        String url =
+            baseUrl + username + '/${list[i].toString().toLowerCase()}';
+
+        ///rc
         print(accToken);
         Map<String, String> header = {"Authorization": "Bearer $accToken"};
         http.Response response = await http.post(url, headers: header);
@@ -129,14 +138,14 @@ class _CartState extends State<Cart> {
         print(response.body);
         print(url);
         print(response.statusCode);
-        if(response.statusCode==200){
-          String msg=jsonDecode(response.body)['message'];
-          if(msg==null){
-            amt=amt+jsonDecode(response.body)['price'];
+        if (response.statusCode == 200) {
+          String msg = jsonDecode(response.body)['message'];
+          if (msg == null) {
+            amt = amt + jsonDecode(response.body)['price'];
           }
         }
       }
-      if(amt!=0) {
+      if (amt != 0) {
         var options = {
           'key': 'rzp_test_8OXCvHsV5OiOpe',
           'amount': amt,
@@ -148,7 +157,7 @@ class _CartState extends State<Cart> {
           }
         };
         // _razorpay.open(options);
-      }else{
+      } else {
         Fluttertoast.showToast(msg: "Events already registered");
       }
     } else {
@@ -160,38 +169,48 @@ class _CartState extends State<Cart> {
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return load == true
-        ? Container(
-        color: Colors.transparent,
-        child: Center(
-          child: Container(
-            child: loader1,
-            color: Colors.transparent,
-          ),)
-    )
-        :
-          Container(
-      height: MediaQuery.of(context).size.height,
-      decoration: list.length==0?BoxDecoration(
-
-        color: Colors.grey[900],
-          image: DecorationImage(
-      image: AssetImage("images/emptycart.png"),
-            fit: BoxFit.scaleDown,
-
+        ? Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              leading: Builder(
+                builder: (context) {
+                  return IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () => Navigator.pop(context),
+                  );
+                },
+              ),
+              title: Column(
+                children: [
+                  Text(' Your Cart'),
+                  
+                ],
+              ),
+              backgroundColor: primary,
+            ),
+            body: Container(
+                color: notiBackColor,
+                child: Center(
+                  child: Container(
+                    child: loader1,
+                  ),
+                )),
           )
-       // gradient: LinearGradient(colors: [Color.fromRGBO(0, 0, 0, 0), Color.fromRGBO(0, 0, 0, 0)], stops: [0, 1])
-      ) :BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("images/ballonback01.jpg"),
-            fit: BoxFit.cover,
-          )
-      ),
-            child:Scaffold(
-              backgroundColor: Colors.transparent,
+        : Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: list.length == 0
+                ? BoxDecoration(
+                    color: notiBackColor,
+                    image: DecorationImage(
+                      image: AssetImage("images/emptycart.png"),
+                      fit: BoxFit.scaleDown,
+                    )
+                    // gradient: LinearGradient(colors: [Color.fromRGBO(0, 0, 0, 0), Color.fromRGBO(0, 0, 0, 0)], stops: [0, 1])
+                    )
+                : BoxDecoration(color: notiBackColor),
+            child: Scaffold(
+                backgroundColor: Colors.transparent,
                 appBar: AppBar(
                   centerTitle: true,
                   leading: Builder(
@@ -205,27 +224,32 @@ class _CartState extends State<Cart> {
                   title: Column(
                     children: [
                       Text(' Your Cart'),
-                      Text(' ${list.length} items ', style: TextStyle(fontSize: 12, color: Colors.white70),)
+                      Text(
+                        ' ${list.length} items ',
+                        style: TextStyle(fontSize: 12, color: Colors.white70),
+                      )
                     ],
                   ),
-                  backgroundColor: drawerBackgroundColor,
+                  backgroundColor: primary,
                 ),
                 body: Container(
                   child: ListView(
                     children: [
                       ListView.builder(
                         itemBuilder: (BuildContext context, int pos) {
-                          AssetImage ig=eventimages[eventName.indexOf(list[pos],0)];
+                          AssetImage ig =
+                              eventimages[eventName.indexOf(list[pos], 0)];
                           return Card(
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(8),
                             ),
-                            margin: EdgeInsets.fromLTRB(10,7,10,7),
+                            margin: EdgeInsets.fromLTRB(10, 7, 10, 7),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
                                     SizedBox(
                                       width: 88,
@@ -235,7 +259,8 @@ class _CartState extends State<Cart> {
                                           padding: EdgeInsets.all(10),
                                           decoration: BoxDecoration(
                                             color: drawerBackgroundColor,
-                                            borderRadius: BorderRadius.circular(8),
+                                            borderRadius:
+                                                BorderRadius.circular(8),
                                           ),
                                           child: Image.asset(ig.assetName),
                                         ),
@@ -243,23 +268,28 @@ class _CartState extends State<Cart> {
                                     ),
                                     SizedBox(width: 20),
                                     Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
                                       children: [
                                         Text(
                                           list[pos],
-                                          style: TextStyle(color: Colors.black, fontSize: 16),
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 16),
                                           maxLines: 2,
                                         ),
                                         SizedBox(height: 10),
                                         Text(
-                                        prices1.containsKey(list[pos])?"\u20B9 "+prices1[list[pos]].toString():"\u20B9 "+"120",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w600, color: Colors.black),
-                                          ),
-
+                                          prices1.containsKey(list[pos])
+                                              ? "\u20B9 " +
+                                                  prices1[list[pos]].toString()
+                                              : "\u20B9 " + "120",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                              color: Colors.black),
+                                        ),
                                       ],
                                     ),
-
                                   ],
                                 ),
                                 IconButton(
@@ -282,85 +312,86 @@ class _CartState extends State<Cart> {
                     ],
                   ),
                 ),
-                bottomNavigationBar: list.length==0?Container():Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 15,
-                    horizontal: 30,
-                  ),
-                  // height: 174,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, -15),
-                        blurRadius: 20,
-                        color: Colors.black.withOpacity(1),
-                      )
-                    ],
-                  ),
-                  child: SafeArea(
-
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-
-                      children: [
-                        SizedBox(
-                          height: 50,
-
-                          child: ListView(
-                              scrollDirection: Axis.horizontal,
-
-                              children: children1,
-                          ),
+                bottomNavigationBar: list.length == 0
+                    ? Container()
+                    : Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 30,
                         ),
-                          SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        // height: 174,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(0, -15),
+                              blurRadius: 20,
+                              color: Colors.black.withOpacity(1),
+                            )
+                          ],
+                        ),
+                        child: SafeArea(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text.rich(
-                                TextSpan(
-                                  text: "Total:\n",
-                                  children: [
-                                    TextSpan(
-                                      text: "\u20B9 "+ sum.toString(),
-                                      style: TextStyle(fontSize: 16, color: Colors.black),
-                                    ),
-                                  ],
+                              SizedBox(
+                                height: 50,
+                                child: ListView(
+                                  scrollDirection: Axis.horizontal,
+                                  children: children1,
                                 ),
                               ),
-                              SizedBox(
-                                width: 170,
-                                child: RaisedButton(
-                                  onPressed: () async {
-                                    await pay();
-                                  },
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 20, vertical: 10),
-                                    child: Text(
-                                      'Register',
-                                      style: TextStyle(color: Colors.white),
+                              SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text.rich(
+                                    TextSpan(
+                                      text: "Total:\n",
+                                      children: [
+                                        TextSpan(
+                                          text: "\u20B9 " + sum.toString(),
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.black),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  color: drawerBackgroundColor,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.only(
-                                          topLeft: Radius.circular(10.0),
-                                          bottomRight: Radius.circular(10.0))),
-                                ),
+                                  SizedBox(
+                                    width: 170,
+                                    child: RaisedButton(
+                                      onPressed: () async {
+                                        await pay();
+                                      },
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 20, vertical: 10),
+                                        child: Text(
+                                          'Register',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ),
+                                      color: drawerBackgroundColor,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(10.0),
+                                              bottomRight:
+                                                  Radius.circular(10.0))),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ],
                           ),
-                        ],
-                      ),
-                    ),
-                  )
-                ),
+                        ),
+                      )),
           );
   }
 }
